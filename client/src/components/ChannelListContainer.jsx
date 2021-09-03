ee0886c54988744ad612577f9cb5f3647f33a448
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-09-02 20:00:20 -0400

    create channel search component structure

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -35,6 +35,7 @@ const ChannelListContainer = () => {
       <SideBar />
       <div className="channel-list__list__wrapper">
         <CompanyHeader />
+        <ChannelSearch />
       </div>
     </>
   );
