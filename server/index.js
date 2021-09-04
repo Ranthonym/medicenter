789d407942f083d586d96ab5005f857a5fba9ce7
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-09-04 14:09:05 -0400

    create controllers for login and signup features

--- server/index.js
+++ server/index.js
@@ -6,14 +6,17 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
+const authRoutes = require("./routes/auth.js");
+
 //middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-//routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
