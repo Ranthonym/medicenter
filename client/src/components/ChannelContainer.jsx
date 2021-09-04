779c4538aa9ac4c8d6a9013e86802185c9190d30
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-09-04 19:47:27 -0400

    implement channel team messaging feature using Stream built in components

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Channel, useChatContext, MessageTeam } from "stream-chat-react";
 
-const ChannelContainer = () => {
-  return <div>ChannelContainer</div>;
+import { ChannelInner, CreateChannel, EditChannel } from "./";
+
+const ChannelContainer = ({
+  isCreating,
+  setIsCreating,
+  isEditing,
+  setIsEditing,
+  createType,
+}) => {
+  const { channel } = useChatContext();
+
+  if (isCreating) {
+    return (
+      <div className="channel__container">
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    );
+  }
+
+  if (isEditing) {
+    return (
+      <div className="channel__container">
+        <EditChannel setIsEditing={setIsEditing} />
+      </div>
+    );
+  }
+
+  const EmptyState = () => (
+    <div className="channel-empty__container">
+      <p className="channel-empty__first">Beginning of your chat history</p>
+      <p className="channel-empty__second">
+        Send texts, links, files, gifs, emojis, anything you want
+      </p>
+    </div>
+  );
+
+  return (
+    <div className="channel__container">
+      <Channel
+        EmptyStateIndicator={EmptyState}
+        Message={(messageProps, index) => (
+          <MessageTeam key={index} {...messageProps} />
+        )}
+      >
+        <ChannelInner setIsEditing={setIsEditing} />
+      </Channel>
+    </div>
+  );
 };
 
 export default ChannelContainer;
