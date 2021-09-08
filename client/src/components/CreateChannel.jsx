8527aaeefbfa2dbe7001f3ca9eca6e7c85aae81b
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-09-07 20:18:25 -0400

    create dynamic user list of all users in db

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
 
-const CreateChannel = () => {
-  return <div></div>;
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
+
+const ChannelNameInput = ({ channelName = "", setChannelName }) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setChannelName(e.target.value);
+  };
+
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input
+        value={channelName}
+        onChange={handleChange}
+        placeholder="channel-name"
+      />
+      <p>Add members</p>
+    </div>
+  );
+};
+
+const CreateChannel = ({ createType, setIsCreating }) => {
+  const [channelName, setChannelName] = useState("");
+
+  return (
+    <div className="create-channel__container">
+      <div className="create-channel__header">
+        <p>{createType === "team" ? "Create a new channel" : "Send a DM"}</p>
+        <CloseCreateChannel setIsCreating={setIsCreating} />
+      </div>
+      {createType === "team" && (
+        <ChannelNameInput
+          channelName={channelName}
+          setChannelName={setChannelName}
+        />
+      )}
+      <UserList />
+    </div>
+  );
 };
 
 export default CreateChannel;
