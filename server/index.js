0f0cae570d693ca8e7a9cdda8bc05d75c49c2bbb
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-09-15 14:59:48 -0400

    implement Twilio sms feature for offline users

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 5000;
 
 const authRoutes = require("./routes/auth.js");
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+
+const twilioClient = require("twilio")(accountSid, authToken);
+
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -14,6 +20,31 @@ app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  const { message, user: sender, type, members } = req.body;
+
+  if (type === "message.new") {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log("Message sent"))
+            .catch((error) => console.log(error));
+        }
+      });
+
+    return res.status(200).send("Message sent");
+  }
+
+  return res.status(200).send("Not a new message request");
 });
 
 app.use("/auth", authRoutes);
